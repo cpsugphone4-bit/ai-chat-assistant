c9470417fca3469b6c3fafb583f4d8aebf6eced3
cpsugphone4-bit <cpsugphone4@gmail.com>
2025-11-02 08:50:51 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // Sesuaikan dengan nama file kamu
-import './index.css';
+import App from '/src/App.jsx'; // Sesuaikan dengan nama file kamu
+import '/src/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
