cd45598b77dd0ad80cc969c4f1972bd4044f9515
cpsugphone4-bit <cpsugphone4@gmail.com>
2025-11-02 08:43:12 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app.jsx'; // Sesuaikan dengan nama file kamu
+import App from './App.jsx'; // Sesuaikan dengan nama file kamu
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
